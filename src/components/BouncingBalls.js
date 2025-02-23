bf57d51700294f26b7374271fb222741b67fe877
jovaughn96 <jovaughna.stephens@gmail.com>
2025-02-23 11:23:43 -0500

    Update from Lovable AI

---
+++ src/components/BouncingBalls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BouncingBalls = () => {
+  return (
+    <div className='bouncing-balls'>
+      {/* Add your bouncing ball animation here */}
+    </div>
+  );
+};
+
+export default BouncingBalls;
